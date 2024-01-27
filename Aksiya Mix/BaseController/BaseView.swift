c4f47e3dce317275844543d622e813e682ed4fed
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2024-01-27 08:35:40 +0500

    1

---
+++ Aksiya Mix/BaseController/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 1/26/24.
+//
+
+import Foundation
---
+++ Aksiya Mix/Customs/SearchView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchView.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 1/26/24.
+//
+
+import Foundation
