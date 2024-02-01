63f1b5ea484f612dbffbfd6d9f43a2061b809d16
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2024-02-01 19:42:55 +0500

    selectable view in progress

---
+++ Aksiya Mix/SingleProduct/View/Views/SelectableView.swift
@@ -0,0 +1,21 @@
+//
+//  SelectableView.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 2/1/24.
+//
+
+import UIKit
+
+class SelectableView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+}
