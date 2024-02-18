c14fa975cc97454647bcd450330cb62c8232749f
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2024-02-18 21:18:18 +0500

    back

---
+++ Aksiya Mix/Aksiya Mix/TabbarControllers/User/UserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserViewController.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 18/02/24.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
