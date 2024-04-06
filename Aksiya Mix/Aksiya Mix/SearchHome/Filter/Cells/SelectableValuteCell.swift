36a7cb8e56a7f277cee1cb99e7a63e83ff1fe567
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2024-04-07 03:27:11 +0500

    SelectableValuteCell is complated

---
+++ Aksiya Mix/Aksiya Mix/SearchHome/Filter/Cells/SelectableValuteCell.swift
@@ -0,0 +1,86 @@
+//
+//  SelectableValuteCell.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 07/04/24.
+//
+
+import UIKit
+
+class SelectableValuteCell: UICollectionViewCell {
+    
+    lazy var titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "Valyutani tanlang"
+        lbl.font = .appFont(ofSize: 16, weight: .semibold)
+        return lbl
+    }()
+    
+    lazy var uzsButton: BlueButton = {
+        let btn = BlueButton()
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = .selectBlue
+        btn.setTitle("🇺🇸 USD", for: .normal)
+        btn.titleLabel?.font = .appFont(ofSize: 14, weight: .medium)
+        return btn
+    }()
+    
+    lazy var usdButton: BlueButton = {
+        let btn = BlueButton()
+        btn.backgroundColor = .white
+        btn.setTitle("🇺🇿 UZS", for: .normal)
+        btn.titleLabel?.font = .appFont(ofSize: 14, weight: .medium)
+        return btn
+    }()
+
+    lazy var mainStack: UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 12
+        stack.axis = .vertical
+        stack.alignment = .leading
+        return stack
+    }()
+    
+    lazy var buttonsStack: UIStackView = {
+        let stack = UIStackView()
+        stack.spacing = 6
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setUI() {
+        contentView.backgroundColor = .backColor
+        contentView.addSubview(mainStack)
+        [titleLabel, buttonsStack].forEach { item in
+            mainStack.addArrangedSubview(item)
+        }
+        [uzsButton, usdButton].forEach { item in
+            buttonsStack.addArrangedSubview(item)
+        }
+        
+    }
+    
+    private func setConstraints() {
+        mainStack.snp.makeConstraints { make in
+            make.top.bottom.equalTo(contentView).inset(4)
+            make.left.right.equalTo(contentView).inset(16)
+        }
+        usdButton.snp.makeConstraints { make in
+            make.height.equalTo(40)
+            make.width.equalTo(100)
+        }
+    }
+    
+}
--- Aksiya Mix/Aksiya Mix/SearchHome/Filter/SearchFilterView.swift
+++ Aksiya Mix/Aksiya Mix/SearchHome/Filter/SearchFilterView.swift
@@ -23,6 +23,8 @@ class SearchFilterView: UIView {
         collection.showsVerticalScrollIndicator = false
         collection.register(SelectableCityCell.self, forCellWithReuseIdentifier: "SelectableCityCell")
         collection.register(SelectableFilterCell.self, forCellWithReuseIdentifier: "SelectableFilterCell")
+        collection.register(SelectableValuteCell.self, forCellWithReuseIdentifier: "SelectableValuteCell")
+        collection.register(SelectablePrinceCell.self, forCellWithReuseIdentifier: "SelectablePrinceCell")
         collection.register(SelectableCategoryCell.self, forCellWithReuseIdentifier: "SelectableCategoryCell")
         return collection
     }()
@@ -56,23 +58,39 @@ extension SearchFilterView: UICollectionViewDelegate, UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        10
+        if section == 0 {
+            return 5
+        } else {
+            return 25
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.row == 0 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableFilterCell", for: indexPath) as! SelectableFilterCell
-            
-            return cell
-        } else if indexPath.row == 1 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableCategoryCell", for: indexPath) as! SelectableCategoryCell
-            
-            return cell
-        } else if indexPath.row == 2 {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableCityCell", for: indexPath) as! SelectableCityCell
-            
-            return cell
-
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableFilterCell", for: indexPath) as! SelectableFilterCell
+                
+                return cell
+            } else if indexPath.row == 1 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableCategoryCell", for: indexPath) as! SelectableCategoryCell
+                
+                return cell
+            } else if indexPath.row == 2 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableCityCell", for: indexPath) as! SelectableCityCell
+                
+                return cell
+                
+            } else if indexPath.row == 3 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableValuteCell", for: indexPath) as! SelectableValuteCell
+                
+                return cell
+                
+            } else if indexPath.row == 4 {
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectablePrinceCell", for: indexPath) as! SelectablePrinceCell
+                
+                return cell
+                
+            }
         }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectableFilterCell", for: indexPath) as! SelectableFilterCell
         
@@ -80,14 +98,21 @@ extension SearchFilterView: UICollectionViewDelegate, UICollectionViewDataSource
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if indexPath.row == 0 {
-            return CGSize(width: 0.screenWight, height: 70)
-        } else if indexPath.row == 1 {
-            return CGSize(width: 0.screenWight, height: 110)
-        } else if indexPath.row == 2 {
-            return CGSize(width: 0.screenWight, height: 90)
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                return CGSize(width: 0.screenWight, height: 70)
+            } else if indexPath.row == 1 {
+                return CGSize(width: 0.screenWight, height: 110)
+            } else if indexPath.row == 2 {
+                return CGSize(width: 0.screenWight, height: 90)
+            } else if indexPath.row == 3 {
+                return CGSize(width: 0.screenWight, height: 80)
+            } else if indexPath.row == 4 {
+                return CGSize(width: 0.screenWight, height: 90)
+            }
         }
         return CGSize(width: 0.screenWight, height: 70)
+        
     }
     
 }
