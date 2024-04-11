a60eb88f4115cd3d78c5fee85a0504272c78d9a2
Ibrohim Begaliev <81876891+ibegaliev@users.noreply.github.com>
2024-04-11 10:15:50 +0500

    added add new market

---
+++ Aksiya Mix/Aksiya Mix/AddNewMarket/AddNewMarketController.swift
@@ -0,0 +1,17 @@
+//
+//  AddNewMarketController.swift
+//  Aksiya Mix
+//
+//  Created by iBegaliev on 11/04/24.
+//
+
+import UIKit
+
+class AddNewMarketController: AksiyaViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
